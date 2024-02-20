10faca4fb65b7d9c68251c1ded0bdbb338945629
chamit6450 <chamit6450@gmail.com>
2024-02-20 23:08:12 +0530

    movie

---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './App.css'
+export default function Navbar({onSearch}) {
+    const [query,setQuery] = useState("")
+  return (
+    <div className='navbar'>
+        <h1>MovieFlix</h1>
+        <ul className='nav'>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/movies">Movie</Link>
+            </li>
+            <li>
+                <Link to="/tv">Tv</Link>
+            </li>
+            <li>
+                <Link to="/favMov">Movie Library</Link>
+            </li>
+            <li>
+                <Link to="/favTv">Tv Library</Link>
+            </li>
+        </ul>
+        <div className='search'>
+            <input type="text" onChange={(e)=>setQuery(e.target.value)}/>
+            <button onClick={()=>onSearch(query)}>Search</button>
+        </div>
+    </div>
+  )
+}
